747510e93c5b66882fd789dd698ac4fb158c57cc
anirban nag <anirban.nag2212@gmail.com>
2024-03-24 00:54:30 +0530

    seconds commit

--- collection/Test.java
+++ collection/Test.java
@@ -2,7 +2,7 @@ package collection;
 
 public class Test {
     public static void main(String[] args) {
-        String str= "Total PACS: 46";
+        String str= "Total PACS: 46()";
         String []str1=str.split(" ");
         System.out.println(str1[2]);
 
